a65e21c3b009f69627b9f841e0d0725b4e131496
Ninoorta <blackcathp9x@gmail.com>
2020-08-24 07:43:20 +0700

    fix name

--- Sign/src/component/parts/Banner/index.js
+++ Sign/src/component/parts/Banner/index.js
@@ -10,7 +10,7 @@ class Banner extends Component {
             < div className="banner" >
                 <div className="container">
                     <div className="col-md-7 col-xs-12">
-                        <h1 className="banner__title">Đăng ký thành viên Luxstay - Tích điểm thưởng và nhận ưu đãi</h1>
+                        <h1 className="banner__title">Đăng ký thành viên TTBStay - Tích điểm thưởng và nhận ưu đãi</h1>
                         <p className="banner__text">Nhanh chóng, tiện lợi và an toàn. Đăng ký liền tay, rinh ngay quyền lợi.</p>
                     </div>
                 </div>
